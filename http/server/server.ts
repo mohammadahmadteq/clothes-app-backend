c2fe228f452944e1d5c2e1c87915e660633a36ed
Mohammad <mohammad---ahmad@outlook.com>
2023-07-19 23:06:17 +0500

    set up ts-node

--- http/server/server.ts
+++ http/server/server.ts
@@ -1,16 +1,21 @@
 import "reflect-metadata";
 
-import fastify from 'fastify'
-import AdsRoutes from '../../http/routes/AdsRoutes';
+import fastify from "fastify";
+import AdsRoutes from "../../http/routes/AdsRoutes";
+import {API_PORT, API_URL} from "../../src/config/serverConfig";
 
-const server = fastify()
+const server = fastify();
 
-server.register(AdsRoutes)
+server.register(AdsRoutes);
 
-server.listen({ port: 3000, host: '0.0.0.0' }, (err, address) => {
-  if (err) {
-    console.error(err)
-    process.exit(1)
-  }
-  console.log(`Server listening at ${address}`)
-})
+const Port: number = API_PORT ? Number(`${API_PORT}`) : 8080;
+const Host: string = API_URL ? API_URL : "0.0.0.0";
+
+server.listen({port: Port, host: Host}, (err, address) => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    console.log(`Server listening at ${address}`);
+});
+
---
+++ src/config/serverConfig.ts
@@ -0,0 +1,8 @@
+import "dotenv/config";
+
+const API_URL = process.env.API_URL;
+const API_VERSION = process.env.API_VERSION;
+const API_PORT = process.env.API_PORT;
+
+export {API_PORT, API_VERSION, API_URL};
+
